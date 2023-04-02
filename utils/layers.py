fc8165d1caecd6eec41108f0617ef884390eb771
ugomezjr <ugomez198@gmail.com>
2023-04-01 21:23:09 -0700

    updated model.py and layers.py

--- utils/layers.py
+++ utils/layers.py
@@ -1,5 +1,32 @@
 from torch import nn
 
+class PatchEmbedding(nn.Module):
+  """Turns a 2D input image into a 1D sequence learnable embedding vector.
+
+  Args: 
+    in_channels (int): Number of color channels for the input images. Defaults to 3.
+    patch_size (int): Size of patches to convert input image into. Defaults to 16.
+    embedding_dim (int): Size of embedding to turn image into. Defaults to 768.
+  """
+  def __init__(self, 
+               in_channels: int=3, 
+               patch_size: int=16, 
+               embedding_dim: int=768):
+    super().__init__()
+
+    self.conv = nn.Conv2d(in_channels=in_channels,
+                      out_channels=embedding_dim,
+                      kernel_size=patch_size,
+                      stride=patch_size,
+                      padding=0)
+                      
+    self.flatten = nn.Flatten(start_dim=2,
+                              end_dim=3)
+    
+  def forward(self, x):
+    return self.flatten(self.conv(x)).permute(0, 2, 1)
+
+
 class MultiHeadSelfAttention(nn.Module):
   def __init__(self, 
                embedding_dim: int=768,
@@ -47,4 +74,28 @@ class MultilayerPerceptron(nn.Module):
 
   def forward(self, x):
     return self.mlp(self.layer_norm(x))
+  
+
+class TransformerEncoder(nn.Module):
+  def __init__(self, 
+               embedding_dim: int=768,
+               mlp_size: int=3072,
+               num_heads: int=12,
+               attn_dropout: float=0.0,
+               mlp_dropout: float=0.1):
+    super().__init__()
+    
+    self.multihead_attn = MultiHeadSelfAttention(embedding_dim=embedding_dim, 
+                                                 num_heads=num_heads,
+                                                 attn_dropout=attn_dropout)
+    
+    self.mlp = MultilayerPerceptron(embedding_dim=embedding_dim,
+                                    mlp_size=mlp_size,
+                                    dropout=mlp_dropout)
+    
+
+  def forward(self, x):
+    x = self.multihead_attn(x) + x
+    x = self.mlp(x) + x
+    return x
   
--- utils/model.py
+++ utils/model.py
@@ -1,25 +1,63 @@
+import torch
 from torch import nn
-from layers import MultiHeadSelfAttention, MultilayerPerceptron
+from layers import PatchEmbedding, TransformerEncoder
 
-class TransformerEncoder(nn.Module):
+class ViT(nn.Module):
+
   def __init__(self, 
+               height: int=224,
+               width: int=224,
+               color_channels: int=3,
+               patch_size: int=16,
+               batch_size: int=1,
+               num_layers: int=12,
                embedding_dim: int=768,
                mlp_size: int=3072,
                num_heads: int=12,
-               attn_dropout: float=0.0,
-               mlp_dropout: float=0.1):
+               dropout: float=0.1,
+               num_classes: int=1000):
     super().__init__()
-    
-    self.multihead_attn = MultiHeadSelfAttention(embedding_dim=embedding_dim, 
-                                                 num_heads=num_heads,
-                                                 attn_dropout=attn_dropout)
-    
-    self.mlp = MultilayerPerceptron(embedding_dim=embedding_dim,
-                                    mlp_size=mlp_size,
-                                    dropout=mlp_dropout)
-    
 
-  def forward(self, x):
-    x = self.multihead_attn(x) + x
-    x = self.mlp(x) + x
-    return x
+    self.num_patches = int((height * width) / patch_size**2)
+    self.class_embedding = nn.Parameter(data=torch.randn(batch_size, 1, embedding_dim), 
+                                     requires_grad=True)
+    self.position_embedding = nn.Parameter(data=torch.randn(batch_size, self.num_patches + 1, embedding_dim),
+                                           requires_grad=True)
+    self.dropout = nn.Dropout(p=dropout,
+                              inplace=True)
+    
+    self.embedded_patches = PatchEmbedding(in_channels=color_channels,
+                                           patch_size=patch_size,
+                                           embedding_dim=embedding_dim)
+
+    self.transformer_encoder = nn.Sequential(*[TransformerEncoder(embedding_dim=embedding_dim,
+                                                                  mlp_size=mlp_size,
+                                                                  num_heads=num_heads,
+                                                                  attn_dropout=0.0,
+                                                                  mlp_dropout=dropout) for _ in range(num_layers)])
+    
+    self.mlp_head = nn.Sequential(
+        nn.LayerNorm(normalized_shape=embedding_dim),
+        nn.Linear(in_features=embedding_dim, 
+                  out_features=num_classes)
+    )
+
+  def forward(self, x: torch.Tensor) -> torch.Tensor:
+    batch_size = x.shape[0]
+
+    xp = self.embedded_patches(x)
+
+    xp = torch.cat((self.class_embedding, xp), dim=1)
+    
+    xp += self.position_embedding
+
+    xp = self.dropout(xp)
+
+    out = self.transformer_encoder(xp)
+    
+    return self.mlp_head(out[:, 0, :])
+  
+if __name__ == "__main__":
+  image = torch.randn(3, 224, 224)
+  model = ViT()
+  print(model(image.unsqueeze(0)).shape)
